5ff92636fca3c744bedf32b1c1b4dc7d43b02604
Lucas Ramos <lucasrcdias@gmail.com>
2017-11-09 22:25:43 -0200

    Set a better format to errors and remove password when user created

--- api/users/index.js
+++ api/users/index.js
@@ -4,10 +4,12 @@ const usersRoutes = (api) => {
   api.post('/users', (req, res) => {
     User.create(req.body.user)
       .then((user) => {
+        delete user.password
+
         res.status(201).send(user)
       })
       .catch((user) => {
-        res.status(400).send(user)
+        res.status(400).send({ 'errors': user.errors })
       })
   })
 }
