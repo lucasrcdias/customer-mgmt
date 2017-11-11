fc2345255f44a7060ddb9454bda556001ec7fedb
Lucas Ramos <lucasrcdias@gmail.com>
2017-11-11 20:45:48 -0200

    Get current user on authentication middleware

--- middlewares/authentication/index.js
+++ middlewares/authentication/index.js
@@ -1,5 +1,6 @@
-const jwt    = require('jsonwebtoken')
-const config = require('config')
+const jwt         = require('jsonwebtoken')
+const config      = require('config')
+const CurrentUser = require('./../../services/current-user')
 
 const isBypassed = (req) => {
   const bypassed = [
@@ -21,25 +22,23 @@ const isBypassed = (req) => {
 const authMiddleware = (req, res, next) => {
   if (isBypassed(req)) { return next() }
 
-  const token = req.header('Authorization')
-
-  console.log('[AUTHENTICATION] Authenticating...')
+  const token = req.header('Authorization').replace('Bearer ', '')
 
   jwt.verify(token, config.get('secret'), (error, decoded) => {
-    if (error) {
-      console.log('[AUTHENTICATION] Authentication failure :(')
+    if (error || !decoded) {
+      console.error('[AUTHENTICATION] Authentication failed')
 
-      return res.status(401).send({
-        user: {
-          errors: {
-            token: 'O token informado não é válido'
-          }
-        }
-      })
+      return res.status(401).send({ 'errors': { 'token': 'O token informado não é válido' } })
     }
 
-    console.log('[AUTHENTICATION] Authenticated :)')
-    next()
+    console.log('[AUTHENTICATION] Authentication success')
+
+    CurrentUser.fromToken(decoded)
+      .then((user) => {
+        req.currentUser = user
+        next()
+      })
+      .catch((error) => { return res.status(401).send(error) })
   })
 }
 
