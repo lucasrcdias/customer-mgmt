058a1e3a32b3dda08bb3f8cd3f3fc695b3b06a8f
Lucas Ramos <lucasrcdias@gmail.com>
2017-11-11 20:45:04 -0200

    Use database record to sign jwt token without password

--- services/auth.js
+++ services/auth.js
@@ -1,6 +1,7 @@
 const jwt    = require('jsonwebtoken')
 const config = require('config')
 const bcrypt = require('bcrypt')
+const _      = require('lodash')
 const User   = require('./../models/user')
 
 const Authenticate = (user) => {
@@ -11,11 +12,14 @@ const Authenticate = (user) => {
           return reject({ 'errors': { 'user': 'Usuário não encontrado' } })
         }
 
+        record = record.get({ plain: true })
+
         bcrypt.compare(user.password, record.password)
           .then((result) => {
             if (result) {
-              return jwt.sign(user, config.get('secret'), { expiresIn: '1d' }, (error, token) => {
+              return jwt.sign(_.omit(record, ['password']), config.get('secret'), { expiresIn: '1d' }, (error, token) => {
                 if (error) {
+                  console.error('[JWT ERROR] ' + error)
                   return reject({ 'errors': { 'token': 'Não foi possível gerar o token de autenticação' } })
                 }
 
