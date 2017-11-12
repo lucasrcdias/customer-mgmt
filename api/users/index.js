2a33f2924750573c89c46342f29e0a94fec4b255
Lucas Ramos <lucasrcdias@gmail.com>
2017-11-12 00:12:58 -0200

    Return model instance instead of simple object

--- api/users/index.js
+++ api/users/index.js
@@ -1,12 +1,11 @@
+const _    = require('lodash')
 const User = require('./../../models/user')
 
 const usersRoutes = (api) => {
   api.post('/users', (req, res) => {
     User.create(req.body.user)
       .then((user) => {
-        delete user.password
-
-        res.status(201).send(user)
+        res.status(201).send(_.omit(user, ['password']))
       })
       .catch((user) => {
         res.status(400).send({ 'errors': user.errors })
