6bb2d3adc1a84afac042f91ff954ca633fd2d374
Lucas Ramos <lucasrcdias@gmail.com>
2017-11-13 10:03:29 -0200

    Create initial structure to project messages

---
+++ messages/index.js
@@ -0,0 +1,9 @@
+const Errors        = require('./errors')
+const Validations   = require('./validations')
+const Confirmations = require('./confirmations')
+
+module.exports = {
+  Errors,
+  Validations,
+  Confirmations
+}
