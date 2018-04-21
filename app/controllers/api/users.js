4445128c7c53b5fbdf66e7c6946c59b2ec0a1e8b
Lucas Ramos <lucasrcdias@gmail.com>
2018-04-20 23:36:55 -0300

    :sparkles: Add /me endpoint

--- app/controllers/api/users.js
+++ app/controllers/api/users.js
@@ -3,6 +3,11 @@ const User = require('./../../models/user')
 const Authenticate = require('./../../services/auth')
 
 const usersRoutes = (api) => {
+  api.get('/me', (req, res) => {
+    const currentUser = _.omit(req.currentUser.get({ 'plain': true }), ['password']);
+    res.status(200).send(currentUser);
+  });
+
   api.post('/users', (req, res) => {
     User.create(req.body.user)
       .then((user) => {
@@ -14,12 +19,12 @@ const usersRoutes = (api) => {
           })
           .catch((error) => {
             res.status(400).send(error)
-          })        
+          })
       })
       .catch((user) => {
         res.status(400).send({ 'errors': user.errors })
       })
-  })
+  });
 }
 
 module.exports = usersRoutes
